2c553d94141acace19eb00f03e2b14a3c69e0d31
alilopez <alzunun@upchiapas.edu.mx>
2021-01-13 10:14:41 -0600

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hola from './Hola'
+
+class App extends React.Component{
+    render() {
+        return (
+            <Hola />
+            )
+    }
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+
+module.exports = {
+    mode: 'development',
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: "/public"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-react"],
+                        plugins: ["react-hot-loader/babel"]
+                    }
+                }
+            }
+        ]
+    }
+}
